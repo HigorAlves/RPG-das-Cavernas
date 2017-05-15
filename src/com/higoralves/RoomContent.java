e32a34ba2de8ad1d5fd2f2fa186d6a686622fe7f
Higor Alves <Goodyearalves@gmail.com>
2017-05-15 18:48:26 -0300

    RoomContent

--- src/com/higoralves/RoomContent.java
+++ src/com/higoralves/RoomContent.java
@@ -3,7 +3,9 @@ package com.higoralves;
 public enum RoomContent {
 	/*SALA00(int Portas,int PortaAberta,int PortaPoção, int qtTrolls, int qtOuro, int qtDiamante, int qtMachado, int qtPoção)*/
 	
-	SALA01(2,0,0,0,0,0,0,0),SALA02(2,0,0,0,0,0,0,0),SALA03(2,0,0,0,0,0,0,0),SALA04(2,0,0,0,0,0,0,0),SALA05(2,0,0,0,0,0,0,0),SALA06(2,0,0,0,0,0,0,0),SALA07(2,0,0,0,0,0,0,0),SALA08(2,0,0,0,0,0,0,0),SALA09(2,0,0,0,0,0,0,0),SALA10(2,0,0,0,0,0,0,0),SALA11(2,0,0,0,0,0,0,0),SALA12(3,0,0,0,0,0,0,0),SALA13(2,0,0,0,0,0,0,0),SALA14(2,0,0,0,0,0,0,0),SALA15(2,0,0,0,0,0,0,0),SALA16(2,0,0,0,0,0,0,0),SALA17(2,0,0,0,0,0,0,0),SALA18(2,0,0,0,0,0,0,0),SALA19(2,0,0,0,0,0,0,0),SALA20(2,0,0,0,0,0,0,0);
+	SALA01(2,0,0,0,0,0,0,0),SALA02(2,0,0,0,0,0,0,0),SALA03(2,0,0,0,0,0,0,0),SALA04(2,0,0,0,0,0,0,0),SALA05(2,0,0,0,0,0,0,0),SALA06(2,0,0,0,0,0,0,0),SALA07(2,0,0,0,0,0,0,0),
+	SALA08(2,0,0,0,0,0,0,0),SALA09(2,0,0,0,0,0,0,0),SALA10(2,0,0,0,0,0,0,0),SALA11(2,0,0,0,0,0,0,0),SALA12(3,0,0,0,0,0,0,0),SALA13(2,0,0,0,0,0,0,0),SALA14(2,0,0,0,0,0,0,0),
+	SALA15(2,0,0,0,0,0,0,0),SALA16(2,0,0,0,0,0,0,0),SALA17(2,0,0,0,0,0,0,0),SALA18(2,0,0,0,0,0,0,0),SALA19(2,0,0,0,0,0,0,0),SALA20(2,0,0,0,0,0,0,0);
 	
 	private int qtPortas, portaAberta, portaPocao, qtTrolls, qtOuro, qtDiamante, qtMachado, qtPocao;
 	
